ee58d3ba3bdf38346cb9edfa06f6c507558b0fee
MariaSoleMoretti <101416076+MariaSoleMoretti@users.noreply.github.com>
2026-05-26 17:59:15 +0200

    Updated Sidebar.tsx

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -29,7 +29,7 @@ export default function Sidebar({ currentView, onViewChange }: SidebarProps) {
             fontFamily: 'Comfortaa, sans-serif',
           }}
         >
-          PollinAction
+          PolliSight
         </span>
       </div>
 
